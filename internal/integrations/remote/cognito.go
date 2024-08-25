24a479c736dadcacad04d8a64852761d634295c2
Thiago Luis <thiago.luis88@gmail.com>
2024-08-25 18:41:02 -0300

    [WIP] chore: try to auto confirm signup Cognito

--- internal/integrations/remote/cognito.go
+++ internal/integrations/remote/cognito.go
@@ -1,6 +1,8 @@
 package remote
 
 import (
+	"fmt"
+
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	cognito "github.com/aws/aws-sdk-go/service/cognitoidentityprovider"
@@ -57,9 +59,26 @@ func (c *CognitoRemoteDataSourceImpl) SignUp(user *model.Customer) error {
 		},
 	}
 
-	_, err := c.cognitoClient.SignUp(userCognito)
+	result, err := c.cognitoClient.SignUp(userCognito)
 
 	if err != nil {
+		return err
+	}
+
+	output := result.SetUserConfirmed(true)
+
+	print(output)
+
+	// Confirm the user immediately using the temporary password
+	confirmRequest := &cognito.ConfirmSignUpInput{
+		Username: aws.String(user.CPF),
+		ClientId: &c.appClientID,
+	}
+
+	_, err = c.cognitoClient.ConfirmSignUp(confirmRequest)
+
+	if err != nil {
+		fmt.Println("Error confirming user:", err)
 		return err
 	}
 
