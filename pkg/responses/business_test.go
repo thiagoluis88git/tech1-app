eb3753d9082935c068290a1ead39e2bfbd085e23
Thiago Luis <thiago.luis88@gmail.com>
2024-10-26 12:06:23 -0300

    test: increase coverage

--- pkg/responses/business_test.go
+++ pkg/responses/business_test.go
@@ -114,4 +114,69 @@ func TestBusinessResponse(t *testing.T) {
 
 		assert.Equal(t, "Unexpected internal error trying to execute service MOCK - InternalServerError", businessError.Error())
 	})
+
+	t.Run("got NotFound error with Local Error when calling GetResponseError", func(t *testing.T) {
+		t.Parallel()
+
+		err := &responses.LocalError{
+			Code:    responses.NOT_FOUND_ERROR,
+			Message: "NotFound",
+		}
+
+		businessError := responses.GetResponseError(err, "MOCK")
+
+		assert.Equal(t, http.StatusNotFound, businessError.(*responses.BusinessResponse).StatusCode)
+	})
+
+	t.Run("got StatusConflict error with Local Error when calling GetResponseError", func(t *testing.T) {
+		t.Parallel()
+
+		err := &responses.LocalError{
+			Code:    responses.DATABASE_CONFLICT_ERROR,
+			Message: "StatusConflict",
+		}
+
+		businessError := responses.GetResponseError(err, "MOCK")
+
+		assert.Equal(t, http.StatusConflict, businessError.(*responses.BusinessResponse).StatusCode)
+	})
+
+	t.Run("got StatusServiceUnavailable error with Local Error when calling GetResponseError", func(t *testing.T) {
+		t.Parallel()
+
+		err := &responses.LocalError{
+			Code:    responses.DATABASE_ERROR,
+			Message: "StatusServiceUnavailable",
+		}
+
+		businessError := responses.GetResponseError(err, "MOCK")
+
+		assert.Equal(t, http.StatusServiceUnavailable, businessError.(*responses.BusinessResponse).StatusCode)
+	})
+
+	t.Run("got StatusUnprocessableEntity error with Local Error when calling GetResponseError", func(t *testing.T) {
+		t.Parallel()
+
+		err := &responses.LocalError{
+			Code:    responses.DATABASE_CONSTRAINT_ERROR,
+			Message: "StatusUnprocessableEntity",
+		}
+
+		businessError := responses.GetResponseError(err, "MOCK")
+
+		assert.Equal(t, http.StatusUnprocessableEntity, businessError.(*responses.BusinessResponse).StatusCode)
+	})
+
+	t.Run("got StatusUnprocessableEntity error with BusinessResponse Error when calling GetResponseError", func(t *testing.T) {
+		t.Parallel()
+
+		err := &responses.BusinessResponse{
+			StatusCode: 422,
+			Message:    "StatusUnprocessableEntity",
+		}
+
+		businessError := responses.GetResponseError(err, "MOCK")
+
+		assert.Equal(t, http.StatusUnprocessableEntity, businessError.(*responses.BusinessResponse).StatusCode)
+	})
 }
